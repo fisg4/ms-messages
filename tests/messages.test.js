9b682f76b16909488032302ab960f69b89078fc4
jorandmol <jorandmol.dev@gmail.com>
2022-12-20 21:21:55 +0100

    Make test for message text update

--- tests/messages.test.js
+++ tests/messages.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable */ 
+const { ObjectId } = require('mongodb');
 const request = require('supertest');
 
 const app = require('../src/app');
@@ -15,13 +16,16 @@ describe('Test messages API', () => {
       id: messageId, userId: 'usertest', roomId: 'roomtest', text: 'test'
     });
 
-    const getByIdMock = jest.spyOn(Message, 'getById');
+    var getByIdMock;
+
+    beforeEach(() => {
+      getByIdMock = jest.spyOn(Message, 'getById');
+    });
   
     it('Should return OK when searching with an existing id', () => {
       getByIdMock.mockImplementation(async (messageId) => Promise.resolve(message));
 
       return request(app).get(`${BASEPATH}/messages/${messageId}`).then((response) => {
-        console.log(response);
         expect(response.status).toBe(200);
         expect(response.body.content.text).toBe(message.text);
         expect(getByIdMock).toBeCalledWith(messageId);
@@ -33,7 +37,6 @@ describe('Test messages API', () => {
       getByIdMock.mockImplementation(async (wrongId) => Promise.resolve(null));
 
       return request(app).get(`${BASEPATH}/messages/wrongId`).then((response) => {
-        console.log(response);
         expect(response.status).toBe(404);
         expect(getByIdMock).toBeCalledWith(wrongId);
       });
@@ -46,12 +49,116 @@ describe('Test messages API', () => {
       });
 
       return request(app).get(`${BASEPATH}/messages/wrongId`).then((response) => {
-        console.log(response);
         expect(response.status).toBe(500);
         expect(getByIdMock).toBeCalledWith(wrongId);
       });
     });
 
+  });
+
+  describe('PATCH messages/:id', () => {
+    const messageId = 'idtest';
+    const newText = 'updated test text';
+    const userId = '637d0c328a43d958f6ff661f';
+
+    var getByIdMock;
+    var updateTextMock;
+
+    const message = new Message({
+      id: messageId, userId: ObjectId(userId), roomId: 'roomtest', text: 'test text'
+    });
+    const updatedMessage = new Message({
+      id: messageId, userId: ObjectId(userId), roomId: 'roomtest', text: newText
+    });
+
+    beforeEach(() => {
+      getByIdMock = jest.spyOn(Message, 'getById');
+      updateTextMock = jest.spyOn(Message.prototype, 'updateText');
+    });
+
+    it('Should return OK when updating', () => {
+      getByIdMock.mockImplementation(async (messageId) => Promise.resolve(message));
+      updateTextMock.mockImplementation(async (newText) => Promise.resolve(updatedMessage));
+
+      return request(app)
+              .patch(`${BASEPATH}/messages/${messageId}`)
+              .set('userId', userId)
+              .send({ text: newText })
+              .then((response) => {
+                expect(response.status).toBe(200);
+                expect(response.body.content.text).toBe(newText);
+                expect(updateTextMock).toBeCalledWith(newText);
+                expect(getByIdMock).toBeCalledWith(messageId);
+              });
+    });
+
+    it('Should return BAD REQUEST when there is not the required header', () => {
+      getByIdMock.mockImplementation(async (messageId) => Promise.resolve(message));      
+
+      return request(app)
+              .patch(`${BASEPATH}/messages/${messageId}`)
+              .send({ text: newText })
+              .then((response) => {
+                expect(response.status).toBe(400);
+                expect(getByIdMock).toBeCalledWith(messageId);
+              });
+    });
+
+    it('Should return BAD REQUEST when new text is empty', () => {
+      getByIdMock.mockImplementation(async (messageId) => Promise.resolve(message));
+      updateTextMock.mockImplementation(async (undefined) => Promise.reject({ errors: 'error'}));
+
+      return request(app)
+              .patch(`${BASEPATH}/messages/${messageId}`)
+              .send({ text: newText })
+              .then((response) => {
+                expect(response.status).toBe(400);
+                expect(getByIdMock).toBeCalledWith(messageId);
+              });
+    });
+
+    it('Should return FORBIDDEN when the user is not the author', () => {
+      getByIdMock.mockImplementation(async (messageId) => Promise.resolve(message));      
+
+      return request(app)
+              .patch(`${BASEPATH}/messages/${messageId}`)
+              .set('userId', 'wrongUserId')
+              .send({ text: newText })
+              .then((response) => {
+                expect(response.status).toBe(403);
+                expect(getByIdMock).toBeCalledWith(messageId);
+              });
+    });
+
+    it('Should return NOT FOUND when searching with an unexisting id', () => {
+      const wrongId = 'wrongId';
+      getByIdMock.mockImplementation(async (wrongId) => Promise.resolve(null));
+
+      return request(app)
+              .patch(`${BASEPATH}/messages/${messageId}`)
+              .set('userId', userId)
+              .send({ text: newText })
+              .then((response) => {
+                expect(response.status).toBe(404);
+                expect(getByIdMock).toBeCalledWith(wrongId);
+              });
+    });
+
+    it('Should return SERVER ERROR when problems occur', () => {
+      getByIdMock.mockImplementation(async (messageId) => Promise.resolve(message));
+      updateTextMock.mockImplementation(async (newText) => {
+        throw new Error('Server error');
+      });
+
+      return request(app)
+              .patch(`${BASEPATH}/messages/${messageId}`)
+              .set('userId', userId)
+              .send({ text: newText })
+              .then((response) => {
+                expect(response.status).toBe(500);
+                expect(getByIdMock).toBeCalledWith(messageId);
+              });
+    });
   });
 
 });
