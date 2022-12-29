3d4b2f8c93b5162fcb834d9e1eda092929c511b1
jorandmol <jorandmol.dev@gmail.com>
2022-12-29 14:44:06 +0100

    Complete component test for Message and include integration ones

--- src/db.js
+++ src/db.js
@@ -2,18 +2,17 @@
 const mongoose = require('mongoose');
 
 const ENV = process.env.NODE_ENV || 'dev';
-const MONGODB_URI = process.env.DB_URL;
+const MONGODB_URI = process.env.DB_URL || 'mongodb://localhost:27017';
 const MONGODB_CONFIG = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   dbName: ENV
 };
 
-mongoose.connect(MONGODB_URI, MONGODB_CONFIG)
-  .then(() => {
-    console.log('✅ Successfully connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error(`⛔ Ups! Something went wrong with MongoDB connection: ${err.message}`);
-    process.exit(1);
-  });
+mongoose.connect(MONGODB_URI, MONGODB_CONFIG);
+const db = mongoose.connection;
+
+db.on('connected', () => console.info('✅ Successfully connected to MongoDB'));
+db.on('error', (err) => console.error(`⛔ Ups! Something went wrong with MongoDB connection: ${err.message}`));
+
+module.exports = db;
